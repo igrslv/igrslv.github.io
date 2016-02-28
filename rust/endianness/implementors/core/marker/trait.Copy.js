03e3a58c328b7f671c7d4ccaf34042a5ee6ac659
Igor Solovyov <igorsolovyov@me.com>
2016-02-28 14:28:03 +0100

    rust-endianness documentaion

---
+++ rust/endianness/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['endianness'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='endianness/enum.ByteOrder.html' title='endianness::ByteOrder'>ByteOrder</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='endianness/enum.Error.html' title='endianness::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
